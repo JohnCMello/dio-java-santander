934c4ab7d10a3bfab294864bfda0c0647206c636
JohnCMello <johncharlesmello@gmail.com>
2024-05-28 16:39:07 -0300

    application.properties

---
+++ spring-boot/introduction/src/main/java/springbootcourse/configuration_properties/ApplicationProperties.java
@@ -0,0 +1,11 @@
+package springbootcourse.configuration_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationProperties {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationProperties.class, args);
+	}
+}
---
+++ spring-boot/introduction/src/main/java/springbootcourse/properties_value/SistemaMensagem.java
@@ -0,0 +1,35 @@
+package springbootcourse.properties_value;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem implements CommandLineRunner {
+	@Value("${teste:valor atribuido a propriedade caso chave invalida, test por exemplo, em application.properties}")
+	private String teste;
+	@Value("${nome:NoReply-DIO}")
+	private String nome;
+	@Value("${email}")
+	private String email;
+	@Value("${telefones}")
+	private List<Long> telefones =
+			new ArrayList<>(Arrays.asList(new Long[]{11956781254L}));
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Mensagem enviada por: " + nome
+				+ "\nE-mail:" + email
+				+ "\nCom telefones para contato: " + telefones);
+		System.out.println("Seu cadastro foi aprovado");
+		
+		System.out.println("#############################");
+		System.out.println(teste);
+		System.out.println("#############################");
+		
+	}
+}
